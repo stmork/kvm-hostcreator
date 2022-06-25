f81c415015adcc612864e4f29945104d172fbdb8
Steffen A. Mork <linux-dev@morknet.de>
2022-06-25 12:27:03 +0200

    Include license and README files into debian package.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -15,6 +15,10 @@ sed -i\
    -e "s/%BUILD%/${BUILD_NUMBER}/g"\
    ${BUILD}/DEBIAN/control
 
+mkdir -p          ${BUILD}/usr/share/doc/${PACKAGE}/
+cp -a README.md   ${BUILD}/usr/share/doc/${PACKAGE}/
+cp -a LICENSE.md  ${BUILD}/usr/share/doc/${PACKAGE}/copyright
+
 VERSION=`grep Version ${BUILD}/DEBIAN/control | cut -d" " -f2`
 fakeroot dpkg -b ${BUILD} ${PACKAGE}_${VERSION}_all.deb
 
